e68717918ad1dc5985bf522af6b256a97c232211
Robin van Leur <robinvanleur777@gmail.com>
2024-01-27 09:21:45 +0100

    basic Flask code

--- flask_app.py
+++ flask_app.py
@@ -2,7 +2,6 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/')
 def home():    
-    return "<p>Hello world!</p>"
-
+    return "<p>Baldur's Gate 3</p>"
 if __name__ == '__main__':   
     app.run(port=5000,debug=True)
